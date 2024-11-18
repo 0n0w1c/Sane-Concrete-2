1c7173b1683ec1cb6c28e28146b5892aa9036862
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-11-18 08:25:08 -0600

    Version 1.1.1

--- data-updates.lua
+++ data-updates.lua
@@ -1,26 +1,30 @@
--- Special thanks to Gotlag for these recipe functions
 local function replace_ingredient(ingredients, old_ingredient, new_ingredient)
-    for i,ingredient in pairs(ingredients) do
+    for i, ingredient in pairs(ingredients) do
         if ingredient.name then
             if ingredient.name == old_ingredient then
                 ingredient.name = new_ingredient
             end
-        elseif ingredient[1] == old_ingredient then
-            ingredient[1] = new_ingredient
         end
     end
 end
 
+local function replace_results(results, old_result, new_result)
+    for i, result in pairs(results) do
+        if result.name then
+            if result.name == old_result then
+                result.name = new_result
+            end
+        end
+    end
+end
 
-local function update_recipe(recipe, old_ingredient, new_ingredient)
+local function update_recipe(recipe, old_item, new_item)
     if data.raw.recipe[recipe].ingredients then
-        replace_ingredient(data.raw.recipe[recipe].ingredients, old_ingredient, new_ingredient)
+        replace_ingredient(data.raw.recipe[recipe].ingredients, old_item, new_item)
     end
-    if data.raw.recipe[recipe].normal then
-        replace_ingredient(data.raw.recipe[recipe].normal.ingredients, old_ingredient, new_ingredient)
-    end
-    if data.raw.recipe[recipe].expensive then
-        replace_ingredient(data.raw.recipe[recipe].expensive.ingredients, old_ingredient, new_ingredient)
+
+    if data.raw.recipe[recipe .. "-recycling"].results then
+        replace_results(data.raw.recipe[recipe .. "-recycling"].results, old_item, new_item)
     end
 end
 
