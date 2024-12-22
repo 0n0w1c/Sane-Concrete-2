a36e046d9be19a02e93cb59ba134710376f4fae8
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-12-22 17:19:57 -0600

    Version 1.1.3

--- data-updates.lua
+++ data-updates.lua
@@ -8,23 +8,9 @@ local function replace_ingredient(ingredients, old_ingredient, new_ingredient)
     end
 end
 
-local function replace_results(results, old_result, new_result)
-    for i, result in pairs(results) do
-        if result.name then
-            if result.name == old_result then
-                result.name = new_result
-            end
-        end
-    end
-end
-
 local function update_recipe(recipe, old_item, new_item)
     if data.raw.recipe[recipe] and data.raw.recipe[recipe].ingredients then
         replace_ingredient(data.raw.recipe[recipe].ingredients, old_item, new_item)
-    end
-
-    if data.raw.recipe[recipe .. "-recycling"] and data.raw.recipe[recipe .. "-recycling"].results then
-        replace_results(data.raw.recipe[recipe .. "-recycling"].results, old_item, new_item)
     end
 end
 
@@ -33,3 +19,5 @@ update_recipe("concrete", "iron-ore", "iron-stick")
 if mods["Dectorio"] then
     update_recipe("dect-concrete-grid", "iron-ore", "iron-stick")
 end
+
+if mods["quality"] then require("__quality__.data-updates") end
